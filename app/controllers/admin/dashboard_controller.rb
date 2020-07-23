e4d928f6eac395f04bb8c2c14b669a83f7d4243b
Brian Tran <98brian.tran@gmail.com>
2020-07-22 18:37:33 -0700

    Admin dashboard now shows how many produts and how many categories are in the db

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @product_count = Product.all.count
+    @category_count = Category.all.count
+    puts @products.inspect
   end
 end
