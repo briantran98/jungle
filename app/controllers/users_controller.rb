e5b3f2212e1b89500f29607185732de2ef7330aa
Brian Tran <98brian.tran@gmail.com>
2020-07-23 16:42:02 -0700

    Added all the login checking and adding to database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 	def create
 		user = User.new(user_params)
 		if user.save
-			session[:user_id] = user.user_id
+			session[:user_id] = user.id
 			redirect_to '/'
 		else
 			redirect_to '/signup'
@@ -22,5 +22,6 @@ class UsersController < ApplicationController
 			:password,
 			:password_confirmation
 			)
+	end
 
 end
